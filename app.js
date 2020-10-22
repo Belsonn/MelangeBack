74d0b2f7e86817dff664c1b50ff53a239fe37561
Mateusz Belka <mateusz.belka14@gmail.com>
2020-10-22 20:25:50 +0200

    dodano routing do rejestracji

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 dotenv.config({path: './config.env'});
 app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
 
 
 module.exports = app;
