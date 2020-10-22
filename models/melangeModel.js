18552aab20392e056af2f4a7d44256fb78fd5cac
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-10-22 19:01:49 +0200

    poprawka melangeModel

--- models/melangeModel.js
+++ models/melangeModel.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const crypto = require('crypto');
-// const User = require("./userModel");
-// const Product = require("./productModel");
+
 
 const melangeSchema = new mongoose.Schema({
   name: {
@@ -30,25 +28,6 @@ const melangeSchema = new mongoose.Schema({
       uppercase: true
   }
   
-});
-
-melangeSchema.pre(/^find/, function (next) {
-  this.select("-__v");
-  
-  this.populate({
-    path: "products users",
-    select: "-__v -email",
-  });
-  
-  next();
-});
-
-melangeSchema.pre("save", function (next) {
-  if (this.isNew) {
-    this.createdAt = Date.now();
-    this.inviteToken = crypto.randomBytes(3).toString('hex');
-  }
-  next();
 });
 
 const Melange = mongoose.model("Melange", melangeSchema);
