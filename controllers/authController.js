8c4d5189ad255b24e444034722d60ad1cffd6cb4
Mateusz Belka <mateusz.belka14@gmail.com>
2020-10-24 20:23:05 +0200

    dodano funkcje logowania i funkcje protect

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,52 +5,107 @@ const { promisify } = require("util");
 const globalError = require("./../utils/globalError");
 
 const signToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-  };
-  
-  const createSendToken = (user, statusCode, res) => {
-    const token = signToken(user._id);
-    const expiresIn = new Date(
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const expiresIn = new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  );
+  const cookieOptions = {
+    expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    );
-    const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    };
-    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-  
-    res.cookie("jwt", token, cookieOptions);
-  
-    user.password = undefined;
-  
-    res.status(statusCode).json({
-      status: "success",
-      token,
-      expiresIn,
-      data: {
-        user,
-      },
-    });
+    ),
+    httpOnly: true,
   };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    expiresIn,
+    data: {
+      user,
+    },
+  });
+};
 
 exports.signup = async (req, res, next) => {
-    const user = await User.findOne().or([
-      { name: req.body.name },
-      { email: req.body.email },
-    ]);
-    if (user) {
-      return next(
-        new globalError("User with this email or name already exists.", 400)
-      );
-    }
-    const newUser = await User.create(req.body);
-  
-    const activateToken = newUser.createActivationToken();
-    await newUser.save({ validateBeforeSave: false });
-  
-    createSendToken(user, 200, res);
-  };
+  const user = await User.findOne().or([
+    { name: req.body.name },
+    { email: req.body.email },
+  ]);
+  if (user) {
+    return next(
+      new globalError("User with this email or name already exists.", 400)
+    );
+  }
+  const newUser = await User.create(req.body);
+
+  const activateToken = newUser.createActivationToken();
+  await newUser.save({ validateBeforeSave: false });
+
+  createSendToken(user, 200, res);
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new globalError("Please provide email and password!", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (
+    !user ||
+    !(await user.correctPassword(password, user.password)) ||
+    !user.active
+  ) {
+    return next(
+      new globalError("Incorrect email, password or account is not active", 401)
+    );
+  }
+
+  createSendToken(user, 200, res);
+};
+
+exports.protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new globalError(
+        "You are not logged in! Please log in to get access.",
+        401
+      )
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new globalError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
+  }
+
+  req.user = currentUser;
+  next();
+};
