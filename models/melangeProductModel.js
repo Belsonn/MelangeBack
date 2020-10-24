6415d85a4f0951961f54099fb10648df2c2040af
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-10-24 16:00:54 +0200

    dodano pole kto kupił

--- models/melangeProductModel.js
+++ models/melangeProductModel.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const melangeProductSchema = new mongoose.Schema({
+  users: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
@@ -8,12 +11,15 @@
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
   },
-  factor: [Number]
+  paidBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users"
+  }
 });
 
 melangeProductSchema.pre(/^find/, function (next) {
   this.populate({
-    path: "product users",
+    path: "product users users",
     select: "-__v -email"
   })
   next();
