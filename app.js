343b6029b738c1a4ac53a74f4896f1ac3d8dcd81
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-10-24 15:47:43 +0200

    dodano brakujace routingi

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const melangeProductRouter = require('./routes/melangeProductRoutes');
+const melangeRouter = require('./routes/melangeRoutes');
 
 const app = express();
 
@@ -8,6 +11,8 @@ dotenv.config({path: './config.env'});
 app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
-
+app.use('/api/v1/product', productRouter);
+app.use('/api/v1/melange', melangeRouter)
+app.use('/api/v1/melangeProduct', melangeProductRouter)
 
 module.exports = app;
