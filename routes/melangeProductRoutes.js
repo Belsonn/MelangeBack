f8897b05b6c68a1765261bd873482fb9597de965
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-10-25 21:07:52 +0100

    dodanie brakujacych ścieżek

--- routes/melangeProductRoutes.js
+++ routes/melangeProductRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const melangeProductController = require('./../controllers/melangeProductController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 router.post('/create', melangeProductController.createMelangeProduct);
+router.post('/update', melangeProductController.updateMelangeProduct)
 router.route("/:id").get(melangeProductController.getMelangeProduct).delete(melangeProductController.deleteMelangeProduct);
 router.route("/").get(melangeProductController.getAllMelangeProducts);
 
--- routes/melangeRoutes.js
+++ routes/melangeRoutes.js
@@ -9,10 +9,14 @@ router.use(authController.protect);
 
 
 router.post('/create', melangeController.createMelange);
-router.route('/:id').get(melangeController.getMelange);
+router.post("/createTempUser", melangeController.createTemporaryUser);
+router.route("/join").post(melangeController.joinMelange);
 router.route('/my').get(melangeController.getMyMelanges);
 router.route('/').get(melangeController.getAllMelanges);
-router.route('/:id').get(melangeController.getMelange);
+router
+  .route("/:id")
+  .get(melangeController.getMelange)
+  .delete(melangeController.deleteMelange);
 
 
 module.exports = router; 
