a257dbbdc557f5971b3663d1e958b875ac7897e6
Mateusz Belka <mateusz.belka14@gmail.com>
2020-10-26 02:46:51 +0100

    protect endpointow dodany

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,11 @@ const userController = require("./../controllers/userController");
 const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+
+router.use(authController.protect);
 router
   .route("/me")
-  .get(authController.protect, userController.getMe, userController.getUser);
+  .get(userController.getMe, userController.getUser);
 router
   .route("/:id")
   .get(userController.getUser)
