b354a1a8013516fc2008dd0315f254fda234ff9e
Mateusz Belka <mateusz.belka14@gmail.com>
2020-10-26 15:55:05 +0100

    poprawki

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
@@ -6,6 +7,7 @@ const melangeProductRouter = require('./routes/melangeProductRoutes');
 const melangeRouter = require('./routes/melangeRoutes');
 
 const app = express();
+app.use(cors());
 
 dotenv.config({path: './config.env'});
 app.use(express.json());
