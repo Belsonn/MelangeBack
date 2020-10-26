84b24b3ee0b8df3bd031740951e06db114751d39
Mateusz Belka <mateusz.belka14@gmail.com>
2020-10-26 15:12:34 +0100

    fix userModel

--- models/userModel.js
+++ models/userModel.js
@@ -76,19 +76,6 @@ userSchema.methods.createActivationToken = function() {
 };
 
 
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
-
-    return JWTTimestamp < changedTimestamp;
-  }
-
-  // False means NOT changed
-  return false;
-};
 
 const User = mongoose.model("User", userSchema);
 
